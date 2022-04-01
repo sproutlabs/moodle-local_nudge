e9788b17b82cf5f7134ab17677927bf17b85c23b
lkearn <liam@sproutlabs.com.au>
2022-04-01 15:20:51 +1100

    Fix: Remove another non-compound import.

--- manage_nudges.php
+++ manage_nudges.php
@@ -31,8 +31,6 @@
 
 use local_nudge\dml\nudge_db;
 
-use function get_string;
-
 require_once(__DIR__ . '/../../config.php');
 require_once($CFG->libdir . '/adminlib.php');
 require_once($CFG->libdir . '/tablelib.php');
